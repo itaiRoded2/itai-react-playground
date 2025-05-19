ba13559190a13d439bc1d4a280433843d748ac29
Itai Roded <itai.roded@gmail.com>
2025-05-19 16:12:25 -0500

    useEffect(() => {   loadFromLocalStorage();   // eslint-disable-next-line react-hooks/exhaustive-deps }, []);

--- src/App3.tsx
+++ src/App3.tsx
@@ -15,6 +15,7 @@ const App3: React.FC = () => {
 
   useEffect(() => {
     loadFromLocalStorage();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {

efca3255c74be701256d193c3be04aa79d06447b
Itai Roded <itai.roded@gmail.com>
2025-05-19 18:57:06 -0500

    rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,6 @@
 import js from "@eslint/js";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import globals from "globals";
@@ -8,10 +10,12 @@ export default [
   {
     files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
     plugins: {
+      "@typescript-eslint": tsPlugin,
       react,
       "react-hooks": reactHooks,
     },
     languageOptions: {
+      parser: tsParser,
       ecmaVersion: 2021,
       sourceType: "module",
       globals: {
@@ -26,9 +30,6 @@ export default [
     },
     rules: {
       // Add custom rules here
-      "react/jsx-uses-vars": "error",
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
     },
   },
 ];

93cccaa73df53b092ef3d6ebd9e3d1df4bb41c5c
Itai Roded <itai.roded@gmail.com>
2025-05-19 19:35:58 -0500

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import { Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
+// import TodoApp from "./pages/TodoApp";
+// import ComingSoon from "./pages/ComingSoon";
 import React, { Suspense } from "react";
-import { lazy } from "react";
-
-// Lazy load components for better performance
-const MainContent = lazy(() => import("./app/pages/MainContent"));
-const ItaiReactApp = lazy(() => import("./app/pages/ItaiReactApp"));
+import ItaiReactApp from "app/pages/ItaiReactApp";
+import MainContent from "app/pages/MainContent";
 
 export default function App() {
   return (
     <div className="flex min-h-screen bg-gray-100">
       <Sidebar />
       <main className="flex-1 p-6">
+        {/* Suspense shows fallback while MainContent is loading */}
         <Suspense fallback={<div className="p-6">Loading content...</div>}>
-          <div className="flex-1 bg-gray-100">
-            <Routes>
-              <Route path="/" element={<MainContent />} />
-              <Route path="/todo" element={<ItaiReactApp />} />
-            </Routes>
-          </div>
+          <main className="flex-1 bg-gray-100">{/* <MainContent /> */}</main>
         </Suspense>
+        <Routes>
+          <Route path="/" element={<MainContent />} />
+          <Route path="/todo" element={<ItaiReactApp />} />
+        </Routes>
       </main>
     </div>
   );

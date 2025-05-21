323e1894d8cd17f682e2b528448cfe5811fab078
Itai Roded <itai.roded@gmail.com>
2025-05-21 10:24:05 -0500

    cleanup

--- src/index.js
+++ src/index.js
@@ -4,12 +4,9 @@ import ReactDOM from "react-dom/client";
 // import ItaiReactApp from "./app/pages/ItaiReactApp";
 import reportWebVitals from "./reportWebVitals";
 //import App from "App";
-import { BrowserRouter } from "react-router-dom";
 import AppWrapper from "AppWrapper";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const basename =
-  process.env.NODE_ENV === "production" ? "/itai-react-playground/" : "/";
 
 root.render(
   <React.StrictMode>
